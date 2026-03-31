3ee489373840472cb3b465b031f48967d7ef89a3
20250990-hash <20250990@sungshin.ac.kr>
2026-03-31 15:39:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,24 @@
-# flag_server.py
 from flask import Flask, request, abort
 import hashlib
-import os
 
 app = Flask(__name__)
 
 REAL_FLAG = "FLAG{I_love_Defense}"
 
-# 🔥 절대 노출되면 안 되는 비밀
-SECRET_KEY = os.urandom(32)
-SECRET_SALT = os.urandom(16)
+# ------------------------
+# 메인 서버와 동일해야 함
+# ------------------------
+SHARED_SECRET_KEY = b'\x93\xfa\x17\xab\xcc\x02\x88\x11\xde\x45\x91\xaf\x33\x77\xb0\xca\x18\x9d\xee\x44\x62\x10\x7b\xc8\x91\x2f\xaa\xbe\x01\x92\xef\x77'
+SHARED_SECRET_SALT = b'\x88\xab\xcd\x19\xfa\x70\x11\x42\xbe\x99\x33\x14\xef\x27\xaa\x55'
 
 def generate_key():
-    return hashlib.sha256(SECRET_KEY).hexdigest()
+    return hashlib.sha256(SHARED_SECRET_KEY).hexdigest()
 
 def generate_token(key):
-    return hashlib.sha256((key + SECRET_SALT.hex()).encode()).hexdigest()
+    return hashlib.sha256((key + SHARED_SECRET_SALT.hex()).encode()).hexdigest()
 
 @app.route("/get_flag", methods=["POST"])
 def get_flag():
-    # 🔥 외부 접근 차단 (중요)
-    if request.remote_addr != "127.0.0.1":
-        abort(403)
-
     key = request.form.get("key")
     token = request.form.get("token")
 
